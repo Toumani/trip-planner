c71a645d0326ca6e1debc571df971d1cb009a754
toumani <toumani49@gmail.com>
2019-06-11 10:06:08 +0100

    feat: fuel price can be set by selecting type

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 import { WiredInput } from "wired-input"
+import TextField from '@material-ui/core/TextField';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+
+const inputStyle = {
+	width: '40%',
+}
+
+const GASOLINE_PRICE = 9.87;
+const PETROL_PRICE = 10.33;
+const DIESEL_PRICE = 15.12; // Je dis n'importe quoi
 
 class App extends React.Component {
 	constructor(props) {
@@ -11,7 +26,8 @@ class App extends React.Component {
 		this.state = {
 			distance: 0,
 			consumption: 3.4,
-			fuelPrice: 10,
+			fuelType: 'gazoline',
+			fuelPrice: GASOLINE_PRICE,
 			price: 0,
 		}
 	}
@@ -32,10 +48,32 @@ class App extends React.Component {
 		});
 	}
 
+	setFuelType = (fuelType) => {
+		const { distance, consumption } = this.state;
+		let fuelPrice = this.state.fuelPrice;
+		switch (fuelType) {
+			case 'gazoline':
+				fuelPrice = GASOLINE_PRICE;
+				break;
+			case 'petrol':
+				fuelPrice = PETROL_PRICE;
+				break;
+			case 'diesel':
+				fuelPrice = DIESEL_PRICE;
+				break;
+		}
+		this.setState({
+			fuelType: fuelType,
+			fuelPrice: fuelPrice,
+			price: ((distance*consumption*fuelPrice).toFixed(8)/100).toFixed(2),
+		});
+	}
+
 	setFuelPrice = (fuelPrice) => {
 		const { distance, consumption } = this.state;
 		this.setState({
 			fuelPrice: fuelPrice,
+			fuelType: 'custom',
 			price: ((distance*consumption*fuelPrice).toFixed(8)/100).toFixed(2),
 		});
 	}
@@ -47,10 +85,6 @@ class App extends React.Component {
 
 	render() {
 		const { price } = this.state;
-		// console.log('Distance: ', distance);
-		// console.log('Consumption: ', consumption);
-		// console.log('Fuel price: ', fuelPrice);
-		console.log(this.state.price);
 		return (
 			<div className="App">
 				<header className="App-header">
@@ -70,41 +104,65 @@ class App extends React.Component {
 				<main className="App-main">
 					<section className="form-section">
 						<div className="form-control">
-							<label>Distance (km)</label>
-							<input
+							{/* <label>Distance (km) :</label> */}
+							<TextField
+								label="Distance (km)"
 								onChange={(e) => {
 									this.setDistance(e.target.value);
 								}}
+								style={inputStyle}
 								value={this.state.distance}
-							></input>
+							/>
 						</div>
 
 						<div className="form-control">
-							<label>Fuel consumption (L/100km)</label>
-							<input
+							{/* <label>Fuel consumption (L/100km) :</label> */}
+							<TextField
+								label="Fuel consumption (L/100km)"
 								onChange={(e) => {
 									this.setConsumption(e.target.value);
 								}}
+								style={inputStyle}
 								value={this.state.consumption}
-							></input>
+							/>
 						</div>
 
+						<FormControl component="fieldset">
+							<FormLabel component="legend">Fuel type</FormLabel>
+							<RadioGroup
+								aria-label="Fuel type"
+								name="fueltype"
+								onChange={(e) => {
+									this.setFuelType(e.target.value);
+								}}
+								value={this.state.fuelType}
+							>
+								<FormControlLabel value="gazoline" control={<Radio />} label="Gazoline" />
+								<FormControlLabel value="petrol" control={<Radio />} label="Petrol" />
+								<FormControlLabel value="diesel" control={<Radio />} label="Diesel" />
+								<FormControlLabel value="custom" control={<Radio />} label="Custom" />
+							</RadioGroup>
+						</FormControl>
+
 						<div className="form-control">
-							<label>Fuel price (DH/L)</label>
-							<input
+							<TextField
+								label="Fuel price (DH/L)"
 								onChange={(e) => {
 									this.setFuelPrice(e.target.value);
 								}}
+								style={inputStyle}
 								value={this.state.fuelPrice}
-							></input>
+							/>
 						</div>
 
 						<div className="form-control">
-							<label>Price (DH)</label>
-							<input
+							{/* <label>Price (DH) :</label> */}
+							<TextField
+								label="Price (DH)"
 								readOnly
+								style={inputStyle}
 								value={price}
-							></input>
+							/>
 						</div>
 					</section>
 				</main>
